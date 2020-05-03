bbe1badcca097d518e03f9514ab7a17c6eea7f6e
Max Kmet <maxkmet@gmail.com>
2020-05-04 01:34:08 +0300

    Updates

---
+++ precache-manifest.7c1ebde058d297179b37fffb76b48d23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6388a773cc25dc053b377940c1a44fd9",
+    "url": "/MaxKmet/MaxKmet.github.io/EnglishGames/index.html"
+  },
+  {
+    "revision": "43c3a3d6e02c3b5c26f4",
+    "url": "/MaxKmet/MaxKmet.github.io/EnglishGames/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b4ee1ed66c27354a0e30",
+    "url": "/MaxKmet/MaxKmet.github.io/EnglishGames/static/js/2.a2a5ac36.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MaxKmet/MaxKmet.github.io/EnglishGames/static/js/2.a2a5ac36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43c3a3d6e02c3b5c26f4",
+    "url": "/MaxKmet/MaxKmet.github.io/EnglishGames/static/js/main.9e9ad21f.chunk.js"
+  },
+  {
+    "revision": "2675f870a5c9929826fa",
+    "url": "/MaxKmet/MaxKmet.github.io/EnglishGames/static/js/runtime-main.c62ba76c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MaxKmet/MaxKmet.github.io/precache-manifest.7977e9f85bca93d1fab24fa4b9cbe5db.js"
+  "/MaxKmet/MaxKmet.github.io/EnglishGames/precache-manifest.7c1ebde058d297179b37fffb76b48d23.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MaxKmet/MaxKmet.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MaxKmet/MaxKmet.github.io/EnglishGames/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
