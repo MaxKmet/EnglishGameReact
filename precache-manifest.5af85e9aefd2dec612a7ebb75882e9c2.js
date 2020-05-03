10693ed495ba2f805fb77b18037c6c059b7825d5
Max Kmet <maxkmet@gmail.com>
2020-05-04 01:16:31 +0300

    Updates

---
+++ precache-manifest.5af85e9aefd2dec612a7ebb75882e9c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ce40e65682eb313700a1a891f67559",
+    "url": "/EnglishGame/index.html"
+  },
+  {
+    "revision": "5b8f1893888d3d590334",
+    "url": "/EnglishGame/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b4ee1ed66c27354a0e30",
+    "url": "/EnglishGame/static/js/2.a2a5ac36.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/EnglishGame/static/js/2.a2a5ac36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8f1893888d3d590334",
+    "url": "/EnglishGame/static/js/main.4ad9da13.chunk.js"
+  },
+  {
+    "revision": "451f1dc1481e7ceb7263",
+    "url": "/EnglishGame/static/js/runtime-main.9c66f277.js"
+  }
+]);
